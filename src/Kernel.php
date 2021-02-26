7ca2497767084d4007db8990a5cfb130cf4ce3ee
Beno!t POLASZEK <bpolaszek@gmail.com>
2021-02-26 14:17:04 +0100

    Feat: Add box config to create PHARs

--- src/Kernel.php
+++ src/Kernel.php
@@ -13,12 +13,12 @@ class Kernel extends BaseKernel
 
     protected function configureContainer(ContainerConfigurator $container): void
     {
-        $container->import('../config/{packages}/*.yaml');
-        $container->import('../config/{packages}/' . $this->environment . '/*.yaml');
+        $container->import(\dirname(__DIR__) . '/config/{packages}/*.yaml');
+        $container->import(\dirname(__DIR__) . '/config/{packages}/' . $this->environment . '/*.yaml');
 
         if (is_file(\dirname(__DIR__) . '/config/services.yaml')) {
-            $container->import('../config/services.yaml');
-            $container->import('../config/{services}_' . $this->environment . '.yaml');
+            $container->import(\dirname(__DIR__) . '/config/services.yaml');
+            $container->import(\dirname(__DIR__) . '/config/{services}_' . $this->environment . '.yaml');
         } elseif (is_file($path = \dirname(__DIR__) . '/config/services.php')) {
             (require $path)($container->withPath($path), $this);
         }
@@ -26,13 +26,19 @@ class Kernel extends BaseKernel
 
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
-        $routes->import('../config/{routes}/' . $this->environment . '/*.yaml');
-        $routes->import('../config/{routes}/*.yaml');
+        $routes->import(\dirname(__DIR__) . '/config/{routes}/' . $this->environment . '/*.yaml');
+        $routes->import(\dirname(__DIR__) . '/config/{routes}/*.yaml');
 
         if (is_file(\dirname(__DIR__) . '/config/routes.yaml')) {
-            $routes->import('../config/routes.yaml');
+            $routes->import(\dirname(__DIR__) . '/config/routes.yaml');
         } elseif (is_file($path = \dirname(__DIR__) . '/config/routes.php')) {
             (require $path)($routes->withPath($path), $this);
         }
     }
+
+    public function getProjectDir()
+    {
+        return \dirname(__DIR__);
+    }
+
 }
